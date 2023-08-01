d2241032ea9e56d0aa2afc876c8c4770e099244a
Fahim Uz Zaman <faheemuzzaman@hotmail.com>
2023-08-01 13:36:06 +0500

    HTML Forms, Tables, List and NodeJS Loops, and Conditions

---
+++ class04/index.js
@@ -0,0 +1,68 @@
+// // var numberOne = "20"
+// // var numberTwo = "10"
+// // var piValue = 3.14987546549
+// // piValue
+// // console.log(numberOne-numberTwo);
+// // console.log(numberOne*numberTwo);
+// // console.log(piValue.toPrecision(6));
+
+// // var rand1to10 = Math.floor(Math.random() * 1000 + 1);
+// // console.log(rand1to10)
+
+// var a = 10;
+// var b = 20;
+
+// // // a-- or a++ phley print phir add/sub
+// // // --a or ++a phley add/sub phir print
+
+// // console.log(a++) // 10
+// // console.log(a++) // 11
+// // console.log(++a) // 13
+// // console.log(a--) // 13
+// // console.log(--a) // 11
+// // console.log(a) // 11
+// // console.log(--a) // 10
+
+// // if(a<b)
+// //     console.log("True")
+// // if(a>b)
+// //     console.log("True")
+
+// var x = 1, y = 2, z = 3;
+
+
+// (a>b)?console.log("True"):console.log("False");
+
+// if(x<y<z){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// }
+// if(z>(y>x)){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// }
+
+// var arrStudent = ["Fahim","Ibad","Waleed","Salar"]
+// var arrStudentwithMr = [];
+
+// arrStudent.sort()
+// console.log(arrStudent)
+// arrStudent.forEach(element => {
+//     arrStudentwithMr.push("Mr. "+element)   
+// });
+
+
+
+// console.log(arrStudentwithMr)
+
+console.log("Fahim".toUpperCase())
+console.log("Fahim".toLowerCase())
+console.log("Fahim".replace('i','ee'))
+console.log("Fahim".indexOf('i'))
+console.log("Fahim".length)
+console.log("This is the first Paragraph which I wrritten in NodeJS".split(" "))
+
